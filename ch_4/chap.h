5821022fc4b41becadb7add33ca033ff78fbb5c1
shuaiwng <shuai.wang2@protonmail.com>
2022-02-19 13:03:57 +0100

    chap4: UDP

---
+++ ch_4/chap.h
@@ -0,0 +1,13 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+#include <stdio.h>
+#include <string.h>
